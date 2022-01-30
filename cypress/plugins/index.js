50dde442d1d4a7cff7cb5063dcb20c2409f2a0a4
Niluka Sripali Monnankulama <39120228+NilukaSripalim@users.noreply.github.com>
2022-01-30 14:50:32 +0530

    Add My new Cypress project

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,3 @@
-/*
- *   Copyright (c) 2021 WSO2 Inc. (http://www.wso2.org)
- *   All rights reserved.
- *   This software is the property of WSO2 Inc. and its suppliers, if any.
- *   Dissemination of any information or reproduction of any material contained
- *   herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
- *   You may not alter or remove any copyright or other notice from copies of this content." 
- */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -20,51 +12,11 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-// winston logger
-const { createLogger, format, transports } = require('winston')
-
-const { combine, timestamp, label, printf } = format
-const myFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${level.toUpperCase()}] ${message}`
-})
-
-const logger = createLogger({
-  format: combine(label({ label: 'test' }), timestamp(), myFormat),
-  transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/info.log' }),
-    new transports.Console({ level: 'error' }),
-  ],
-})
-
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-
-  on('task', {
-    logInfo(msg) {
-      return new Promise((resolve, reject) => {
-        logger.info(msg)
-        resolve('')
-      })
-    },
-
-    logWarn(msg) {
-      return new Promise((resolve, reject) => {
-        logger.warn(msg)
-        resolve('')
-      })
-    },
-
-    logError(msg) {
-      return new Promise((resolve, reject) => {
-        logger.error(msg)
-        resolve('')
-      })
-    },
-  })
 }
